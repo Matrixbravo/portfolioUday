d202e36041836823de67928d23e6ccfc83f23343
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2022-11-29 16:48:16 +0530

    updte

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Rocket from './components/Rocket';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Rocket/>
     </div>
   );
 }
